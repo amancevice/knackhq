876f6cc29689f8e7167a8ee1dfaf1f0bae850471
Alexander Mancevice <amancevice@cargometrics.com>
2017-05-03 16:40:59 -0400

    move to pytest

--- setup.py
+++ setup.py
@@ -1,45 +1,32 @@
 import os
 import re
+import textwrap
 from setuptools import setup
 
-NAME    = "knackhq"
-AUTHOR  = "amancevice"
-EMAIL   = "smallweirdnum@gmail.com"
-DESC    = "Interact with KnackHQ API"
-
-CLASSIFIERS = [
-    "Development Status :: 3 - Alpha",
-    "Intended Audience :: Developers",
-    "Intended Audience :: System Administrators",
-    "License :: OSI Approved :: MIT License",
-    "Operating System :: OS Independent",
-    "Programming Language :: Python :: 2.7",
-    "Topic :: Utilities" ]
-REQUIRES = [
-    "certifi>=2016.2.28",
-    "urllib3>=1.12" ]
-TESTS_REQUIRE = ["nose", "mock"]
-
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 def version():
     search = r"^__version__ *= *['\"]([0-9.]+)['\"]"
-    initpy = read("./%s/__init__.py" % NAME)
+    initpy = open('./knackhq/__init__.py').read()
     return re.search(search, initpy, re.MULTILINE).group(1)
 
-setup(
-    name                 = NAME,
-    version              = version(),
-    author               = AUTHOR,
-    author_email         = EMAIL,
-    packages             = [ NAME ],
-    package_data         = { "%s" % NAME : ['README.md'] },
-    include_package_data = True,
-    url                  = 'http://www.smallweirdnumber.com',
-    description          = DESC,
-    long_description     = read('README.md'),
-    classifiers          = CLASSIFIERS,
-    install_requires     = REQUIRES,
-    tests_require        = TESTS_REQUIRE,
-    test_suite           = "nose.collector" )
+
+setup(name='knackhq',
+      version=version(),
+      author='amancevice',
+      author_email='smallweirdnum@gmail.com',
+      packages=['knackhq'],
+      include_package_data=True,
+      url='http://www.smallweirdnumber.com',
+      description='Interact with KnackHQ API',
+      long_description=textwrap.dedent(
+          '''See GitHub_ for documentation.
+          .. _GitHub: https://github.com/amancevice/knackhq'''),
+      classifiers=['Development Status :: 3 - Alpha',
+                   'Intended Audience :: Developers',
+                   'Intended Audience :: System Administrators',
+                   'License :: OSI Approved :: MIT License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python :: 2.7',
+                   'Topic :: Utilities'],
+      install_requires=['certifi>=2016.2.28',
+                        'requests>=2.11.1'])
