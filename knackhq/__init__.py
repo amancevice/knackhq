b3a7a72fe2dc93cef20a55c13243611c8b454606
Alexander Mancevice <alexander.mancevice@hey.com>
2020-07-12 09:37:58 -0400

    init module fixes

--- knackhq/__init__.py
+++ knackhq/__init__.py
@@ -1,4 +1,12 @@
-""" KnackHQ API Client """
-from .knackhq import KnackApp
+"""
+KnackHQ API Client
+"""
+import pkg_resources
 
-__version__ = '0.1.0'
+from knackhq.knackhq import KnackApp  # noqa: F401
+from knackhq import exceptions  # noqa: F401
+
+try:
+    __version__ = pkg_resources.get_distribution(__package__).version
+except pkg_resources.DistributionNotFound:  # pragma: no cover
+    __version__ = None
